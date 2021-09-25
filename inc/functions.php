99d2e11ad9434325db10ae902bf58ada33581913
Rustam Akhmedov <tutyou1972@gmail.com>
2021-09-25 16:49:30 +0500

    cpurse page payement added

--- inc/functions.php
+++ inc/functions.php
@@ -8,3 +8,17 @@ if (!function_exists('stm_put_log')) {
 		else file_put_contents($file, $data);
 	}
 }
+
+function add_init_lms()
+{
+	wp_dequeue_script('stm-lms-lms');
+	wp_enqueue_script('stm-lms-lms-custom', get_stylesheet_directory_uri() . '/assets/js/lms.js', [], stm_lms_custom_styles_v(), false);
+	wp_localize_script('stm-lms-lms-custom', 'stm_lms_vars', array(
+		'symbol' => STM_LMS_Options::get_option('currency_symbol', '$'),
+		'position' => STM_LMS_Options::get_option('currency_position', 'left'),
+		'currency_thousands' => STM_LMS_Options::get_option('currency_thousands', ','),
+		'wp_rest_nonce' => wp_create_nonce('wp_rest')
+	));
+}
+
+add_action('wp_enqueue_scripts', 'add_init_lms', 11);
--- lms/classes/cart.php
+++ lms/classes/cart.php
@@ -7,6 +7,11 @@ class STM_Custom_LMS_Cart extends STM_LMS_Cart
 	{
 		add_filter('stm-lms-payment-methods', 'STM_Custom_LMS_Cart::stm_lms_payment_methods');
 		add_filter('wpcfto_field_payments', 'STM_Custom_LMS_Cart::stm_lms_settings_payment_methods');
+
+		remove_action('wp_ajax_stm_lms_add_to_cart', 'STM_LMS_Cart::add_to_cart');
+		remove_action('wp_ajax_nopriv_stm_lms_add_to_cart', 'STM_LMS_Cart::add_to_cart');
+		add_action('wp_ajax_stm_lms_add_to_cart', 'STM_Custom_LMS_Cart::stm_add_to_cart');
+		add_action('wp_ajax_nopriv_stm_lms_add_to_cart', 'STM_Custom_LMS_Cart::stm_add_to_cart');
 	}
 
 	public static function stm_lms_payment_methods($payment_methods)
@@ -19,5 +24,32 @@ class STM_Custom_LMS_Cart extends STM_LMS_Cart
 	public static function stm_lms_settings_payment_methods($template)
 	{
 		return get_stylesheet_directory() . '/settings/payments/fields/payments.php';
+	}
+
+	public static function stm_add_to_cart()
+	{
+
+		check_ajax_referer('stm_lms_add_to_cart', 'nonce');
+
+		if (!is_user_logged_in() or empty($_GET['item_id'])) die;
+
+		$item_id = intval($_GET['item_id']);
+		$user = STM_LMS_User::get_current_user();
+		$user_id = $user['id'];
+
+		$r = self::_add_to_cart($item_id, $user_id);
+		$payment_method = (!empty($_REQUEST['payment_method'])) ? sanitize_text_field($_REQUEST['payment_method']) : false;
+		if ($payment_method) {
+			self::payment_processing($payment_method);
+		}
+
+		$r['text'] = __('To\'lovga o\'tish', 'masterstudy-child');
+	//	print_r($r);
+		wp_send_json($r);
+	}
+
+	public static function payment_processing($payment_method)
+	{
+
 	}
 }
